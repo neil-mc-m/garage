1ca0843cafbc58961d88a7ff67a319fc39e1951d
studentneil <neilo2000@gmail.com>
2016-09-10 01:21:21 +0100

    mods to the backend, small changes to frontend search box

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -12,6 +12,7 @@ function showResult(str) {
 
                 for (i=0; i<result.length; i++){
                     var counter = result[i];
+
                     console.log(counter.id);
                     document.getElementById("livesearch").innerHTML = counter.make+" "+counter.model;
                 }
